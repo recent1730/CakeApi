9e1e56eb096b4bd60dad6dbde8cc78baac30d82b
Etinder Kochar <jasleen0711kochar@gmail.com>
2021-04-29 13:27:43 +0530

    Adding files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mx!!is=qa=i+khv+#5@g1w-mtbspz8+uy5p5ui&2y*2xqcqoh2'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -44,13 +46,6 @@ INSTALLED_APPS = [
     
     'rest_framework.authtoken',
     ]
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jastestpass@gmail.com'
-EMAIL_HOST_PASSWORD = 'kxjjfetmhwvthmnq' #past the key or password app here
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'default from email'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CORS_ALLOWED_ORIGINS = (
@@ -69,6 +64,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS =config('EMAIL_USE_TLS')
 
 TEMPLATES = [
     {
@@ -152,10 +154,3 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'yoorusername@gmail.com'
-# EMAIL_HOST_PASSWORD = 'password
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
